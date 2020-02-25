6f06b9e635c5bfb6947d1e77e550007d92331d33
kevingarry97 <kevingarry97@gmail.com>
2020-02-25 14:16:53 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as M from '../assets/materialize/js/materialize.min.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'appAng';
+  options = {};
+
+  ngOnInit() {
+    document.addEventListener('DOMContentLoaded', function() {
+      const elems = document.querySelectorAll('.carousel');
+      const instances = M.Carousel.init(elems, this.options);
+    });
+  }
 }
